64272a81599196cb27078ecd0f99b4b63fadea8e
seeratkaurr <hiiiseerat@gmail.com>
2025-09-17 23:33:09 -0700

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include "Student.hpp"
-
 using namespace std;
 
 class LinkedList {
@@ -12,13 +11,13 @@ public:
         head = nullptr;
     }
 
+    // Insert new student at the end
     void insertStudent(int id, string name, float gpa) {
         Student* newStudent = new Student(id, name, gpa);
 
         if (head == nullptr) {
             head = newStudent;
-        }
-        else {
+        } else {
             Student* temp = head;
             while (temp->next != nullptr) {
                 temp = temp->next;
@@ -27,6 +26,7 @@ public:
         }
     }
 
+    // Delete a student by ID
     bool deleteStudent(int id) {
         Student* temp = head;
         Student* prev = nullptr;
@@ -37,12 +37,11 @@ public:
         }
 
         if (temp == nullptr)
-            return false;
+            return false;  // student not found
 
         if (prev == nullptr) {
-            head = head->next;
-        }
-        else {
+            head = head->next;  // deleting head
+        } else {
             prev->next = temp->next;
         }
 
@@ -63,134 +62,6 @@ public:
 
     void displayAll() {
         if (head == nullptr) {
-            cout << "No students in the list.\n";
-            return;
-        }
+            cout << "N
 
-        Student* temp = head;
-        while (temp != nullptr) {
-            cout << temp->id << " " << temp->name << " " << temp->gpa << " ";
-            if (temp->courseHead == nullptr) {
-                cout << "None";
-            }
-            else {
-                cout << "\nCourses:\n";
-                temp->displayCourses();
-            }
-            cout << "\n";
-            temp = temp->next;
-        }
-    }
-
-    int countStudents() {
-        int count = 0;
-        Student* temp = head;
-        while (temp != nullptr) {
-            count++;
-            temp = temp->next;
-        }
-        return count;
-    }
-
-    bool addCourseToStudent(int id, string courseName, string location) {
-        Student* student = searchStudent(id);
-        if (student == nullptr)
-            return false;
-
-        student->addCourse(courseName, location);
-        return true;
-    }
-};
-
-int main() {
-    LinkedList list;
-    int choice;
-
-    while (true) {
-        cout << "\n1. Insert Student\n"
-            << "2. Delete Student\n"
-            << "3. Search Student\n"
-            << "4. Display All\n"
-            << "5. Count Students\n"
-            << "6. Add a course\n"
-            << "7. Exit\n"
-            << "Enter choice: ";
-        cin >> choice;
-
-        if (choice == 1) {
-            int id;
-            string name;
-            float gpa;
-            cout << "Enter ID: ";
-            cin >> id;
-            cin.ignore();
-            cout << "Enter Name: ";
-            getline(cin, name);
-            cout << "Enter GPA: ";
-            cin >> gpa;
-            list.insertStudent(id, name, gpa);
-
-        }
-        else if (choice == 2) {
-            int id;
-            cout << "Enter ID to delete: ";
-            cin >> id;
-            if (list.deleteStudent(id))
-                cout << "Deleted student with ID " << id << endl;
-            else
-                cout << "Student with ID " << id << " not found.\n";
-
-        }
-        else if (choice == 3) {
-            int id;
-            cout << "Enter ID to search: ";
-            cin >> id;
-            Student* student = list.searchStudent(id);
-            if (student != nullptr) {
-                cout << "Found: " << student->id << " " << student->name << " " << student->gpa << "\nCourses:\n";
-                student->displayCourses();
-            }
-            else {
-                cout << "Student not found.\n";
-            }
-
-        }
-        else if (choice == 4) {
-            cout << "Students in List:\n";
-            list.displayAll();
-
-        }
-        else if (choice == 5) {
-            cout << "Total students: " << list.countStudents() << endl;
-
-        }
-        else if (choice == 6) {
-            int id;
-            string courseName, location;
-            cout << "Enter ID: ";
-            cin >> id;
-            cin.ignore();
-            cout << "Enter Course Name: ";
-            getline(cin, courseName);
-            cout << "Enter location: ";
-            getline(cin, location);
-
-            if (list.addCourseToStudent(id, courseName, location))
-                cout << "Course added.\n";
-            else
-                cout << "Student not found.\n";
-
-        }
-        else if (choice == 7) {
-            cout << "Exiting program.\n";
-            break;
-
-        }
-        else {
-            cout << "Invalid choice. Try again.\n";
-        }
-    }
-
-    return 0;
-}
 
