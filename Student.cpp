5b1270d8b80a16918327d34e2832aec075bafa4f
seeratkaurr <hiiiseerat@gmail.com>
2025-09-17 23:32:27 -0700

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -1,39 +1,38 @@
-// TODO:  Student class methods implementation here.
 #include "Student.hpp"
 #include <iostream>
 using namespace std;
 
 Student::Student(int id, string name, float gpa) {
-	this->id = id;
-	this->name = name;
-	this->gpa = gpa;
-	this->courseH = nullptr;
-	this->next = nullptr;
-
+    this->id = id;
+    this->name = name;
+    this->gpa = gpa;
+    this->courseHead = nullptr;
+    this->next = nullptr;
 }
+
 void Student::addCourse(const string& courseName, const string& location) {
-	Course* newCourse = new Course(courseName, location);
-	if (courseH == nullptr) {
-		courseH = newCourse;
-	} else {
-		Course* temp = courseH;
-		while (temp->next != nullptr) {
-			temp = temp->next;
-		}
-		temp->next = newCourse;
-	}
+    Course* newCourse = new Course(courseName, location);
+    if (courseHead == nullptr) {
+        courseHead = newCourse;
+    } else {
+        Course* temp = courseHead;
+        while (temp->next != nullptr) {
+            temp = temp->next;
+        }
+        temp->next = newCourse;
+    }
 }
 
-	}	
-	void Student::displayCourses() const {
-		if (courseH == nullptr) {
-			cout << "No courses enrolled." << endl;
-			return;
-		}
+void Student::displayCourses() const {
+    if (courseHead == nullptr) {
+        cout << "No courses enrolled." << endl;
+        return;
+    }
 
-		Course* temp = courseH;
-		while (temp != nullptr) {
-			cout << "Course: " << temp->courseName << ", Location: " << temp->location << endl;
-			temp = temp->next;
-		}
-	}
+    Course* temp = courseHead;
+    while (temp != nullptr) {
+        cout << "Course: " << temp->courseName << ", Location: " << temp->location << endl;
+        temp = temp->next;
+    }
+}
+
