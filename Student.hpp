49106698baad9cac02fba4e935e53eae4d32c1d5
seeratkaurr <hiiiseerat@gmail.com>
2025-09-17 23:27:17 -0700

    Update Student.hpp

--- Student.hpp
+++ Student.hpp
@@ -1,1 +1,22 @@
 // TO DO:  Student class definition  here.
+#ifndef STUDENT_HPP
+#define STUDENT_HPP
+
+#include <string>
+#include "Course.hpp"
+using namespace std;
+
+class Student {
+public:
+    int id;
+    string name;
+    float gpa;
+    Course* courseH;     // Head of course list
+    Student* next;
+
+    Student(int id, string name, float gpa);
+    void addCourse(const string& courseName, const string& location);
+    void displayCourses() const;
+};
+
+#endif
