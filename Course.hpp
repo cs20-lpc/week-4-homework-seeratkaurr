227eb298e28fb2030d250e3c7912ecc1c9db1ddd
seeratkaurr <hiiiseerat@gmail.com>
2025-09-17 23:28:05 -0700

    Update Course.hpp

--- Course.hpp
+++ Course.hpp
@@ -1,1 +1,18 @@
 // TODO:  Course class definitins go here.
+#ifndef COURSE_HPP
+#define COURSE_HPP
+
+#include <string>
+using namespace std;
+
+class Course {
+public:
+    string courseName;  
+    string location;
+    Course* next;
+
+    Course();
+    Course(const string& name, const string& loc);
+};
+
+#endif
