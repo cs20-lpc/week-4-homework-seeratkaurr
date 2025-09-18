fa96757a9bb706364b817383ee05c74c1fa0de40
seeratkaurr <hiiiseerat@gmail.com>
2025-09-17 23:28:20 -0700

    Update Course.cpp

--- Course.cpp
+++ Course.cpp
@@ -1,1 +1,14 @@
 // TODO:  Course class methods implementation here.
+#include "Course.hpp"
+
+Course::Course() {
+    courseName = "";
+    location = "";
+    next = nullptr;
+}
+
+Course::Course(const string& name, const string& loc) {
+    courseName = name;
+    location = loc;
+    next = nullptr;
+}
